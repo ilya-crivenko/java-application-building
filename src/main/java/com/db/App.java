bc7aedfd1e13a8aa24197351f74a265732d5c278
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-02-04 10:05:28 +0300

    test

--- src/main/java/com/db/App.java
+++ src/main/java/com/db/App.java
@@ -4,10 +4,13 @@ package com.db;
  * Hello world!
  *
  */
+//Test
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+
+
 }
