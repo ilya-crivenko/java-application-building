40c59e359f4f54b34b3351cb576cdde30f82d330
Ilya Crivenko <crivenko.ilya@gmail.com>
2021-02-06 15:33:30 +0300

    Revert test

--- src/main/java/com/db/App.java
+++ src/main/java/com/db/App.java
@@ -4,13 +4,10 @@ package com.db;
  * Hello world!
  *
  */
-//Test
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
-
-
 }
